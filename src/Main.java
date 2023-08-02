d4c45e2dbf88bd16fd92a2ed0c4d4d518660ba55
Buğra <109425576+bugraozdmr@users.noreply.github.com>
2023-08-02 20:48:38 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,11 @@
 
 public class Main {
     public static void main(String[] args) {
+
         game game1=new game();
+
+        // Çalışmaya başlarken kelimeleri dosyadan yükle
+        game1.loadWordsFromFile();
 
         game1.goodGame();
 
